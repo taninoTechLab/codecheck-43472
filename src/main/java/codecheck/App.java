f31c8d94c46068a8d630f13371400d8dd57afcc9
tie303053 <tie303053@S1711C12618-T1.tisnt.tis.co.jp>
2018-06-18 15:45:14 +0900

    test

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -5,21 +5,16 @@ public class App {
 		for (int i = 0, l = args.length; i < l; i++) {
 			String output = String.format("argv[%s]: %s", i, args[i]);
 			System.out.println(output);
+			if (!args[i].matches("^[0-9]+$")) {
+				System.out.println("invalid");
+				return;
+			}
 
+			int arg = Integer.parseInt(args[i]);
+			if (arg <= 1000 && arg > 0) {
+				System.out.println("smart");
 
-			try {
-				int arg = Integer.parseInt(args[i]);
-				if (arg <= 1000 && arg > 0) {
-					if(args[i].matches("^[0-9]+$")) {
-						System.out.println("smart");
-					} else {
-						System.out.println("invalid");
-					}
-
-				} else {
-					System.out.println("invalid");
-				}
-			} catch (Exception e) {
+			} else {
 				System.out.println("invalid");
 			}
 
