ec4712c24ea440cb326feb8907821edfebd15fc3
tie303053 <tie303053@S1711C12618-T1.tisnt.tis.co.jp>
2018-06-18 15:03:50 +0900

    test

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -5,6 +5,14 @@ public class App {
 		for (int i = 0, l = args.length; i < l; i++) {
 			String output = String.format("argv[%s]: %s", i, args[i]);
 			System.out.println(output);
+
+			if(args[i] == null ) {
+				System.out.println("invalid");
+			} else {
+				int arg = Integer.parseInt(args[i]);
+
+			}
+
 		}
 	}
 }
